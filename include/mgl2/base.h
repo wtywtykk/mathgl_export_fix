689d835f53784156c70eae99022344843ae1ffee
abalakin <abalakin@e5b85019-62d8-4000-a5b5-39c118e1a321>
2012-08-13 13:18:27 +0000

    Remove unused function mgl_set_def_schv(); More accurate use of mgl_min_a

--- include/mgl2/base.h
+++ include/mgl2/base.h
@@ -267,8 +267,6 @@ public:
 	/// Set default color scheme
 	inline void SetDefScheme(const char *colors)
 	{	Txt[1].Set(mgl_have_color(colors)?colors:"BbcyrR");	}
-	/// Set default color scheme
-	void SetDefScheme(HCDT val, const char *colors);
 
 	/// Set number of mesh lines
 	inline void SetMeshNum(int val)	{	MeshNum=val;	}
