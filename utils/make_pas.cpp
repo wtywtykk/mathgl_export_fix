4b035f5617dcf181105c763475a2df3a3b6e5bb9
abalakin <abalakin@e5b85019-62d8-4000-a5b5-39c118e1a321>
2013-11-21 02:47:33 +0000

    Finish with Pascal interface (thanks to M.Barg)

--- utils/make_pas.cpp
+++ utils/make_pas.cpp
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-const char *files[]={"../include/mgl2/base_cf.h","../include/mgl2/data_cf.h", "../include/mgl2/datac_cf.h", "../include/mgl2/cont.h", "../include/mgl2/fit.h", "../include/mgl2/plot.h", "../include/mgl2/surf.h", "../include/mgl2/volume.h", "../include/mgl2/vect.h", "../include/mgl2/prim.h", "../include/mgl2/other.h", "../include/mgl2/canvas_cf.h", "../include/mgl2/addon.h", ""};
+const char *files[] =
+{
+	"../include/mgl2/base_cf.h",
+	"../include/mgl2/data_cf.h",
+	"../include/mgl2/datac_cf.h",
+	"../include/mgl2/cont.h",
+	"../include/mgl2/fit.h",
+	"../include/mgl2/plot.h",
+	"../include/mgl2/surf.h",
+	"../include/mgl2/volume.h",
+	"../include/mgl2/vect.h",
+	"../include/mgl2/prim.h",
+	"../include/mgl2/other.h",
+	"../include/mgl2/canvas_cf.h",
+	"../include/mgl2/addon.h",
+	"" };
 
-const char *head="//**************************************************************************\n\
+const char *head =
+		"//**************************************************************************\n\
 // mgl_pas.pas is part of Math Graphic Library                             *\n\
 // Copyright (C) 2008-2013 Mikhail Barg, Alexey Balakin                    *\n\
 //                                                                         *\n\
@@ -73,6 +89,10 @@ type mreal = double;\n\
 {$ELSE}\n\
 type mreal = real;\n\
 {$IFEND}\n\
+{$IFDEF FPC}\n\
+{$ELSE}\n\
+type QWord = Int64;\n\
+{$ENDIF}\n\n\
 Pmreal = ^mreal;\n\n\
 type TNGLDraw = record\n\
 end;\n\
@@ -86,23 +106,30 @@ type TMGLFormula = record\n\
 end;\n\
 type TMGLFormulaC = record\n\
 end;\n\
+type TMGLDataC = record\n\
+end;\n\
 type HMDR = ^TNGLDraw;\n\
 type HMGL = ^TMGLGraph;\n\
 type HMDT = ^TMGLData;\n\
 type HMPR = ^TMGLParse;\n\
 type PPChar = ^PChar;\n\
 type HMEX = ^TMGLFormula;\n\
-type HAEX = ^TMGLFormulaC;\n\n\
+type HAEX = ^TMGLFormulaC;\n\
+type HADT = ^TMGLDataC;\n\n\
 type Preal = ^single;\n\
 type Pdouble = ^double;\n\
 type Pint = ^integer;\n\
+type dual = record\n\
+re, im: mreal;\n\
+end;\n\
+type Pdual = ^dual;\n\
 type TGSLVector = record\n\
 end;\n\
 type TGSLMatrix = record\n\
 end;\n\
 type PGSLVector = ^TGSLVector;\n\
 type PGSLMatrix = ^TGSLMatrix;\n\n\
-type TMglDrawFunction = function (gr: HMGL; p: pointer): integereger; cdecl;\n\
+type TMglDrawFunction = function (gr: HMGL; p: pointer): integer; cdecl;\n\
 function mgl_create_graph_gl(): HMGL; cdecl; external libmgl;\n\
 function mgl_create_graph_glut(draw: TMglDrawFunction; const title: PChar; par: pointer): HMGL; cdecl; external libmglglut;\n\
 function mgl_create_graph_fltk(draw: TMglDrawFunction; const title: PChar; par: pointer): HMGL; cdecl; external libmglfltk;\n\
@@ -110,160 +137,336 @@ procedure mgl_fltk_run(); cdecl; external libmglfltk;\n\
 function mgl_create_graph_qt(draw: TMglDrawFunction; const title: PChar; par: pointer): HMGL; cdecl; external libmglqt;\n\
 procedure mgl_qt_run(); cdecl; external libmglqt;\n";
 
-const char *parse_name(char *name)
+
+const char *footer =
+"\n\
+\n\
+{$IFDEF MSWINDOWS}\n\
+//*****************************************************************************/\n\
+// Delphi - specific\n\
+//*****************************************************************************/\n\
+procedure mgl_begin();\n\
+procedure mgl_end();\n\
+\n\
+procedure mgl_draw_on_canvas(gr: HMGL; width, height: integer; canvas: TCanvas; switchXY: boolean = false);\n\
+\n\
+implementation\n\
+\n\
+var _FPUCW: word;\n\
+\n\
+procedure mgl_begin();\n\
+ begin\n\
+  _FPUCW := Get8087CW();     // backup current FPU CW\n\
+  Set8087CW(_FPUCW or $3F); // masking all FPU exceptions\n\
+ end;\n\
+\n\
+procedure mgl_end();\n\
+ begin\n\
+  Set8087CW(_FPUCW);         // restore old FPU CW\n\
+ end;\n\
+\n\
+procedure mgl_draw_on_canvas(gr: HMGL; width, height: integer; canvas: TCanvas; switchXY: boolean = false);\n\
+  var i, j: integer;\n\
+      bytes: PByte;\n\
+      col: TColor;\n\
+ begin\n\
+  bytes := mgl_get_rgb(gr);\n\
+\n\
+  if (not switchXY) then\n\
+   for j := 0 to height - 1 do\n\
+    for i := 0 to width - 1 do\n\
+     begin\n\
+      col := 0;\n\
+      col := col or (bytes^);\n\
+      inc(bytes);\n\
+      col := col or (bytes^) shl 8;\n\
+      inc(bytes);\n\
+       col := col or (bytes^) shl 16;\n\
+      inc(bytes);\n\
+      canvas.Pixels[i, j] := col;\n\
+    end\n\
+  else\n\
+   for j := height - 1 downto 0 do\n\
+    for i := 0 to width - 1 do\n\
+     begin\n\
+      col := 0;\n\
+      col := col or (bytes^);\n\
+      inc(bytes);\n\
+      col := col or (bytes^) shl 8;\n\
+      inc(bytes);\n\
+       col := col or (bytes^) shl 16;\n\
+      inc(bytes);\n\
+      canvas.Pixels[j, i] := col;\n\
+     end;\n\
+ end;\n\
+\n\
+{$ENDIF}\n\
+end.\n";
+
+
+bool processArgument(char *dest, const char *arg, const char *prefix, const char *format)
 {
-	static char res[1024];
-	char *ptr,*arg[20],nul=0;	// TODO check if 20 arguments is enough
-	unsigned i,j;
-	for(i=0;name[i]!='(';i++)	res[i]=name[i];
-	res[i]='(';	res[i+1]=0;	i++;
-	while(name[i]<=' ')	i++;
-	for(j=0;j<20;j++)	arg[j]=&nul;
-	for(j=0;j<20;j++)
+	const int prefixLen = strlen(prefix);
+	if ( strncmp(arg, prefix, prefixLen) == 0 )
 	{
-		arg[j]= name[i]<=' ' ? name+i+1 : name+i;
-		ptr=strchr(name+i,',');
-		if(!ptr)	break;
-		*ptr=0;	i=ptr-name+1;
+		char argname[32];
+		strcpy(argname, arg + prefixLen);
+		if (strcmp(argname, "to") == 0)
+		{
+			int argNameLen = strlen(argname);
+			argname[argNameLen] = '_';
+			argname[argNameLen + 1] = 0;
+		}
+		sprintf(dest, format, argname);
+		return true;
 	}
-	ptr=strchr(name+i,')');	if(ptr) *ptr=0;
-	if(arg[0][0]==0)	strcat(res," ");
-	for(j=0;j<20;j++)
+	return false;
+}
+
+const char *parse_name(char *name, bool &needOverload)
+{
+	const int MAX_ARG = 20; // TODO check if 20 arguments is enough
+	static char res[1024];
+	char *ptr, *arg[MAX_ARG], nul = 0;	
+	unsigned i, j;
+	needOverload = false;
+	for ( i = 0; name[i] != '('; ++i )
 	{
-		if(arg[j][0]==0)	break;
-		ptr = res+strlen(res);
-		if(!strncmp(arg[j],"HMGL ",5))
-			sprintf(ptr, "%s: HMGL;", arg[j]+5);
-//		else if(!strncmp(arg[j],"HADT ",5))
-//		{	strcat(res,arg[j]+5);	strcat(res,": HADT;");	}
-		else if(!strncmp(arg[j],"HCDT ",5))
-			sprintf(ptr, "const %s: HMDT;", arg[j]+5);
-		else if(!strncmp(arg[j],"HMDT ",5))
-			sprintf(ptr, "%s: HMDT;", arg[j]+5);
-		else if(!strncmp(arg[j],"HMPR ",5))
-			sprintf(ptr, "%s: HMPR;", arg[j]+5);
-		else if(!strncmp(arg[j],"HMEX ",5))
-			sprintf(ptr, "%s: HMEX;", arg[j]+5);
-		else if(!strncmp(arg[j],"const float *",13))
-			sprintf(ptr, "const %s: Preal;", arg[j]+13);
-		else if(!strncmp(arg[j],"const double *",14))
-			sprintf(ptr, "const %s: Pdouble;", arg[j]+14);
-		else if(!strncmp(arg[j],"mreal *",7))
-			sprintf(ptr, "%s: Pmreal;", arg[j]+7);
-		else if(!strncmp(arg[j],"double *",8))
-			sprintf(ptr, "%s: Pdouble;", arg[j]+8);
-		else if(!strncmp(arg[j],"char *",6))
-			sprintf(ptr, "%s: PChar;", arg[j]+6);
-		else if(!strncmp(arg[j],"int *",5))
-			sprintf(ptr, "%s: Pint;", arg[j]+5);
-		else if(!strncmp(arg[j],"long *",6))	// TODO check
-			sprintf(ptr, "%s: Pint;", arg[j]+6);
-		else if(!strncmp(arg[j],"const char *",12))
-			sprintf(ptr, "const %s: PChar;", arg[j]+12);
-		else if(!strncmp(arg[j],"const wchar_t *",15))
-			sprintf(ptr, "const %s: PWideChar;", arg[j]+15);
-		else if(!strncmp(arg[j],"char ",5))
-			sprintf(ptr, "%s: char;", arg[j]+5);
-		else if(!strncmp(arg[j],"long ",5))		// TODO check
-			sprintf(ptr, "%s: integer;", arg[j]+5);
-		else if(!strncmp(arg[j],"uint32_t ",9))	// TODO check
-			sprintf(ptr, "%s: integer;", arg[j]+9);
-		else if(!strncmp(arg[j],"int ",4))
-			sprintf(ptr, "%s: integer;", arg[j]+4);
-		else if(!strncmp(arg[j],"mreal ",6))
-			sprintf(ptr, "%s: mreal;", arg[j]+6);
-		else if(!strncmp(arg[j],"double ",7))
-			sprintf(ptr, "%s: double;", arg[j]+7);
-		else if(!strncmp(arg[j],"gsl_vector *",12))
-			sprintf(ptr, "%s: PGSLVector;", arg[j]+12);
-		else if(!strncmp(arg[j],"gsl_matrix *",12))
-			sprintf(ptr, "%s: PGSLMatrix;", arg[j]+12);
+		res[i] = name[i];
+	}
+
+//	res[i] = 0;
+//	printf("'%s'\n", res);
+	
+	//TODO: special case, invent some general way to handle overloads, i.e. functions with same names. Still would require re-doing whole parser..
+	if (strncmp(res, " mgl_expi", i - 1) == 0)
+	{
+		needOverload = true;
+	}
+	res[i] = '(';
+	res[i + 1] = 0;
+	++i;
+	while ( name[i] <= ' ' )
+	{
+		++i;
+	}
+	for ( j = 0; j < MAX_ARG; ++j )
+	{
+		arg[j] = &nul;
+	}
+	for ( j = 0; j < MAX_ARG; ++j )
+	{
+		arg[j] = (name[i] <= ' ' ? name + i + 1 : name + i);
+		ptr = strchr(name + i, ',');
+		if ( !ptr )
+		{
+			break;
+		}
+		*ptr = 0;
+		i = ptr - name + 1;
+	}
+	ptr = strchr(name + i, ')');
+	if ( ptr )
+	{
+		*ptr = 0;
+	}
+	if ( arg[0][0] == 0 )
+	{
+		strcat(res, " ");
+	}
+	for ( j = 0; j < MAX_ARG; ++j )
+	{
+		if ( arg[j][0] == 0 )
+		{
+			break;
+		}
+		ptr = res + strlen(res);
+		if ( 		processArgument(ptr, arg[j], "HMGL ", "%s: HMGL;")
+				||  processArgument(ptr, arg[j], "HCDT ",  "const %s: HMDT;")
+				||	processArgument(ptr, arg[j], "HMDT ",  "%s: HMDT;")
+				||	processArgument(ptr, arg[j], "HADT ",  "%s: HADT;")
+				||	processArgument(ptr, arg[j], "HAEX ",  "%s: HAEX;")
+				||	processArgument(ptr, arg[j], "HMPR ",  "%s: HMPR;")
+				||	processArgument(ptr, arg[j], "HMEX ",  "%s: HMEX;")
+				||	processArgument(ptr, arg[j], "const float *",  "const %s: Preal;")
+				||	processArgument(ptr, arg[j], "const double *",  "const %s: Pdouble;")
+				||	processArgument(ptr, arg[j], "mreal *",  "%s: Pmreal;")
+				||	processArgument(ptr, arg[j], "double *",  "%s: Pdouble;")
+				||	processArgument(ptr, arg[j], "char *",  "%s: PChar;")
+				||	processArgument(ptr, arg[j], "int *", "%s: Pint;")
+				||	processArgument(ptr, arg[j], "long *",  "%s: Pint;")
+				||	processArgument(ptr, arg[j], "const char *",  "const %s: PChar;")
+				||	processArgument(ptr, arg[j], "const wchar_t *",  "const %s: PWideChar;")
+				||	processArgument(ptr, arg[j], "char ",  "%s: char;")
+				||	processArgument(ptr, arg[j], "long ",  "%s: integer;")
+				||	processArgument(ptr, arg[j], "uint32_t ",  "%s: LongWord;")
+				||	processArgument(ptr, arg[j], "uint64_t ",  "%s: QWord;")
+				||	processArgument(ptr, arg[j], "int ",  "%s: integer;")
+				||	processArgument(ptr, arg[j], "mreal ",  "%s: mreal;")
+				||	processArgument(ptr, arg[j], "const dual *",  "const %s: Pdual;")
+				||	processArgument(ptr, arg[j], "dual *",  "%s: Pdual;")
+				||	processArgument(ptr, arg[j], "dual ",  "%s: dual;")
+				||	processArgument(ptr, arg[j], "double ",  "%s: double;")
+				||	processArgument(ptr, arg[j], "gsl_vector *",  "%s: PGSLVector;")
+				||	processArgument(ptr, arg[j], "gsl_matrix *",  "%s: PGSLMatrix;")
+			)
+		{
+			//already procedded in processArgument
+		}
 		else
+		{
 			sprintf(ptr, " !!! %s;", arg[j]);
+		}
 	}
 	
-//	if(strncmp(name+i,"HMGL ",5))
-	i = strlen(res);	res[i-1]=')';
+	i = strlen(res);
+	res[i - 1] = ')';
 	return res;
+}
+
+bool processDeclaration(FILE *out, char *declaration, const char *prefix, const char *format)
+{
+	const int prefixLen = strlen(prefix);
+	if ( strncmp(declaration, prefix, prefixLen) == 0 )
+	{
+		bool needOverload = false;
+		fprintf(out, format, parse_name(declaration + prefixLen, needOverload));
+		if (needOverload)
+		{
+			fprintf(out, "overload;\n");
+		}
+		return true;
+	}
+	return false;
 }
 
 bool parse_file(const char *fname, FILE *out)
 {
-	if(!fname || fname[0]==0)	return false;
-	FILE *fp=fopen(fname,"rt");
-	if(!fp)	return false;
+	if ( !fname || fname[0] == 0 )
+	{
+		return false;
+	}
+	FILE *fp = fopen(fname, "rt");
+	if ( !fp )
+	{
+		return false;
+	}
+	
+	fprintf(out, "{== %s ==}\n", fname);
 	
 	char buf[1024], *ptr;
-	while(!feof(fp))
+	while ( !feof(fp) )
 	{
-		fgets(buf,1024,fp);
+		fgets(buf, 1024, fp);
 		// first filter unwanted strings
-		if(buf[0]==0 || buf[0]=='\n' || buf[1]=='\n')	continue;
-		if(buf[0]=='#' || buf[0]=='}')	continue;
-		if(!strncmp(buf, "extern",6))	continue;
-		if(!strncmp(buf, "class",5))	continue;
-		if(!strncmp(buf, "struct",6))	continue;
-		if(!strncmp(buf, "typedef",7))	continue;
-		if(strstr(buf, "void *"))	continue;
-		if(strstr(buf, "_("))	continue;
-		if(strstr(buf, "FILE"))	continue;
-		if(strstr(buf, "TODO"))	continue;
-		if(strstr(buf, "...)"))	continue;
-		
-		// TODO following 5 lines enable later
-		if(strstr(buf, "* const *"))	continue;
-		if(strstr(buf, "uint64_t"))	continue;
-		if(strstr(buf, "dual"))	continue;
-		if(strstr(buf, "HADT"))	continue;
-		if(strstr(buf, "HAEX"))	continue;
-
-		// now filter comments
-		if(buf[0]=='/' && buf[1]=='*')
+		if ( buf[0] == 0 || buf[0] == '\n' || buf[1] == '\n' )
 		{
-			do fgets(buf,1024,fp);	while(!strstr(buf,"*/"));
 			continue;
 		}
-		ptr = strchr(buf,';');	if(ptr)	*ptr=' ';
-		for(unsigned i=strlen(buf)-1;buf[i]<=' ';i--)	buf[i]=0;
-		if(buf[0]=='/' && buf[1]=='/')
-			fprintf(out,"%s\n",buf);
-		else if(!strncmp(buf,"void MGL_EXPORT",15))
-			fprintf(out,"procedure %s; cdecl; external libmgl;\n",parse_name(buf+16));
-		else if(!strncmp(buf,"int MGL_EXPORT",14))
-			fprintf(out,"function %s: integer; cdecl; external libmgl;\n",parse_name(buf+15));
-		else if(!strncmp(buf,"double MGL_EXPORT",17))
-			fprintf(out,"function %s: double; cdecl; external libmgl;\n",parse_name(buf+18));
-		else if(!strncmp(buf,"mreal MGL_EXPORT",16))
-			fprintf(out,"function %s: mreal; cdecl; external libmgl;\n",parse_name(buf+17));
-		else if(!strncmp(buf,"long MGL_EXPORT",15))
-			fprintf(out,"function %s: integer; cdecl; external libmgl;\n",parse_name(buf+16));
-		else if(!strncmp(buf,"HMDT MGL_EXPORT",15))
-			fprintf(out,"function %s: HMDT; cdecl; external libmgl;\n",parse_name(buf+16));
-		else if(!strncmp(buf,"HMGL MGL_EXPORT",15))
-			fprintf(out,"function %s: HMGL; cdecl; external libmgl;\n",parse_name(buf+16));
-		else if(!strncmp(buf,"MGL_EXPORT const char *",23))
-			fprintf(out,"function %s: PChar; cdecl; external libmgl;\n",parse_name(buf+24));
-		else if(!strncmp(buf,"MGL_EXPORT mreal *",18))
-			fprintf(out,"function %s: Pmreal; cdecl; external libmgl;\n",parse_name(buf+19));
-		else if(!strncmp(buf,"MGL_EXPORT const unsigned char *",32))
-			fprintf(out,"function %s: PByte; cdecl; external libmgl;\n",parse_name(buf+33));
-		else if(!strncmp(buf,"HMPR MGL_EXPORT",15))
-			fprintf(out,"function %s: HMPR; cdecl; external libmgl;\n",parse_name(buf+16));
-		else if(!strncmp(buf,"HMEX MGL_EXPORT",15))
-			fprintf(out,"function %s: HMEX; cdecl; external libmgl;\n",parse_name(buf+16));
-		else if(!strncmp(buf,"HADT MGL_EXPORT",15))
-			fprintf(out,"function %s: HADT; cdecl; external libmgl;\n",parse_name(buf+16));
+		if ( buf[0] == '#' || buf[0] == '}' )
+		{
+			continue;
+		}
+		if ( !strncmp(buf, "extern", 6) )
+		{
+			continue;
+		}
+		if ( !strncmp(buf, "class", 5) )
+		{
+			continue;
+		}
+		if ( !strncmp(buf, "struct", 6) )
+		{
+			continue;
+		}
+		if ( !strncmp(buf, "typedef", 7) )
+		{
+			continue;
+		}
+		if ( strstr(buf, "void *") )
+		{
+			continue;
+		}
+		if ( strstr(buf, "_(") )
+		{
+			continue;
+		}
+		if ( strstr(buf, "FILE") )
+		{
+			continue;
+		}
+		if ( strstr(buf, "TODO") )
+		{
+			continue;
+		}
+		if ( strstr(buf, "...)") )
+		{
+			continue;
+		}
+		
+		// TODO enable later
+		if ( strstr(buf, "* const *") )
+		{
+			continue;
+		}
+		
+		// now filter comments
+		if ( buf[0] == '/' && buf[1] == '*' )
+		{
+			do
+			{
+				fgets(buf, 1024, fp);
+			}
+			while ( !strstr(buf, "*/") );
+			continue;
+		}
+		ptr = strchr(buf, ';');
+		if ( ptr )
+		{
+			*ptr = ' ';
+		}
+		for ( unsigned i = strlen(buf) - 1; buf[i] <= ' '; i-- )
+		{
+			buf[i] = 0;
+		}
+		if ( buf[0] == '/' && buf[1] == '/' )
+		{
+			fprintf(out, "%s\n", buf);
+		}
+		else if ( 	   processDeclaration(out, buf, "void MGL_EXPORT", 		"procedure %s; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "int MGL_EXPORT", 		"function %s: integer; cdecl; external libmgl;\n") 
+					|| processDeclaration(out, buf, "double MGL_EXPORT", 	"function %s: double; cdecl; external libmgl;\n") 
+					|| processDeclaration(out, buf, "mreal MGL_EXPORT", 	"function %s: mreal; cdecl; external libmgl;\n") 
+					|| processDeclaration(out, buf, "long MGL_EXPORT", 		"function %s: integer; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "dual MGL_EXPORT", 		"function %s: dual; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "MGL_EXPORT dual *", 	"function %s: PDual; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "HMDT MGL_EXPORT", 		"function %s: HMDT; cdecl; external libmgl;\n") 
+					|| processDeclaration(out, buf, "HMGL MGL_EXPORT", 		"function %s: HMGL; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "MGL_EXPORT const char *", "function %s: PChar; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "MGL_EXPORT mreal *", 	"function %s: Pmreal; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "MGL_EXPORT const unsigned char *", "function %s: PByte; cdecl; external libmgl;\n") 
+					|| processDeclaration(out, buf, "HMPR MGL_EXPORT", 		"function %s: HMPR; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "HMEX MGL_EXPORT", 		"function %s: HMEX; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "HADT MGL_EXPORT", 		"function %s: HADT; cdecl; external libmgl;\n")
+					|| processDeclaration(out, buf, "HAEX MGL_EXPORT", 		"function %s: HAEX; cdecl; external libmgl;\n")
+				)
+		{
+			//already processed by processDeclaration
+		}
 		else
-			fprintf(out,"!!!!\t%s\n",buf);	// NOTE should be never here!
+		{
+			fprintf(out, "!!!!\t%s\n", buf);	// NOTE should be never here!
+		}
 	}
 	return true;
 }
 
 int main()
 {
-	FILE *fout = fopen("../include/mgl2/mgl_pas.pas","wt");
-	fprintf(fout,"%s\n",head);
-	for(int i=0;parse_file(files[i],fout);i++);
+	FILE *fout = fopen("../include/mgl2/mgl_pas.pas", "wt");
+	fprintf(fout, "%s\n", head);
+	for ( int i = 0; parse_file(files[i], fout); i++ ) {}
+	fprintf(fout, "%s\n", footer);
 	fclose(fout);
 	return 0;
 }
