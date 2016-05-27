2aaf459b3db43fc43bf9e434cd4416f4394640c0
dikis <dikis@e5b85019-62d8-4000-a5b5-39c118e1a321>
2016-05-27 15:52:44 +0000

    MathGL-2x: getting rid of stl in qstring to/from char and wchar_t translation and locale fix, forth upload

--- udav/newcmd_dlg.cpp
+++ udav/newcmd_dlg.cpp
@@ -248,7 +248,8 @@ void NewCmdDialog::nameChanged(int s)
 	// clear old
 	kind->clear();	kinds.clear();	for(k=0;k<NUM_CH;k++)	argn[k].clear();
 	// try to find the keyword
-	const char *cmd = n.toLatin1().constData();
+	QByteArray qcmd = n.toLatin1();
+	const char *cmd = qcmd.constData();
 	if(!parser.CmdType(cmd))	return;
 	info->setText(parser.CmdDesc(cmd));
 
